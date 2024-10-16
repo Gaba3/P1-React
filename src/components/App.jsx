b153edf10ebc29706f05e53bf8e0308e779e3dd5
Gaba3 <00103923@uca.edu.sv>
2024-10-16 01:11:38 -0600

    P1+React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,22 @@
 import Header from './Header';
-import Benefits from './Benefits';
-import Characteristics from './Characteristics';
-import Testimony from './Testimony';
-import Subscribe from './Subscribe';
+import Beneficios from './Beneficios';
+import Caracteristicas from './Caracteristicas';
+import Testimonios from './Testimonios';
+import Suscripcion from './Suscripcion';
 import Animacion from './Animacion'
 import Footer from './Footer';
-import './styles/index.css';
+import '/home/gabriel/Escritorio/Parcial 2/P1-React/P1-React/src/styles/index.css';
+
 
 const App = () => {
   return (
     <div>
       <Header />
       <main>
-        <Benefits />
-        <Characteristics />
-        <Testimony />
-        <Subscribe />
+        <Beneficios />
+        <Caracteristicas />
+        <Testimonios />
+        <Suscripcion />
         <Animacion />
       </main>
       <Footer />
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './src/components/App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '/home/gabriel/Escritorio/Parcial 2/P1-React/P1-React/src/components/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
